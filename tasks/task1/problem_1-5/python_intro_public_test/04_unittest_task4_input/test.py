5ad97e04f4496a98006fcb138c46ca7e7ee41aec
Yuri Tihonov <tihonov-yuri@mail.ru>
2025-09-16 22:13:53 +0300

    Fix method calls

--- tasks/task1/problem_1-5/python_intro_public_test/04_unittest_task4_input/test.py
+++ tasks/task1/problem_1-5/python_intro_public_test/04_unittest_task4_input/test.py
@@ -9,7 +9,7 @@ def test_bank_card():
     assert a.__str__() == "To learn the balance call balance."
     a(50)
     assert a.total_sum == 50
-    assert a.balance == 50
+    assert a.balance() == 50
     assert a.balance_limit == 1
     try:
         a(50)
@@ -17,16 +17,16 @@ def test_bank_card():
         pass
     assert a.total_sum == 0
     a.put(30)
-    assert a.balance == 30
+    assert a.balance() == 30
     try:
-        a.balance
+        a.balance()
     except ValueError:
         pass
 
     b = BankCard(50)
     
     for i in range(100):
-        assert b.balance == 50
+        assert b.balance() == 50
     
     c = BankCard(300, 2)
     d = a + c
