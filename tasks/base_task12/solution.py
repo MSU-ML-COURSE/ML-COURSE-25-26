482a2a3a2ede83eebf21dbc500149335f97b53e4
Bogachevv <dog67688@gmail.com>
2026-05-05 12:00:23 +0300

    Add base12 & research7

---
+++ tasks/base_task12/solution.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+import sklearn
+import sklearn.metrics
+
+
+def silhouette_score(x, labels):
+    '''
+    :param np.ndarray x: Непустой двумерный массив векторов-признаков
+    :param np.ndarray labels: Непустой одномерный массив меток объектов
+    :return float: Коэффициент силуэта для выборки x с метками labels
+    '''
+
+    # Ваш код здесь:＼(º □ º l|l)/
+
+    return sil_score
+
+
+def bcubed_score(true_labels, predicted_labels):
+    '''
+    :param np.ndarray true_labels: Непустой одномерный массив меток объектов
+    :param np.ndarray predicted_labels: Непустой одномерный массив меток объектов
+    :return float: B-Cubed для объектов с истинными метками true_labels и предсказанными метками predicted_labels
+    '''
+
+    # Ваш код здесь:＼(º □ º l|l)/
+
+    return score
